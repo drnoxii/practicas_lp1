eb50bd418755823070f6340bf24f0c12d9e2dd9d
drnoxii <jdconislla@gmail.com>
2026-05-11 13:23:47 -0500

    resolucion de errores

--- src/java/Dao/UsuarioDaoImpl.java
+++ src/java/Dao/UsuarioDaoImpl.java
@@ -7,7 +7,7 @@ package Dao;
 import Interface.IUsuario;
 import Model.Persona;
 import Model.Usuario;
-import com.sun.jdi.connect.spi.Connection;
+import java.sql.*;
 
 /**
  *
@@ -17,14 +17,33 @@ public class UsuarioDaoImpl implements IUsuario {
     private Connection cn;
 
     @Override
-    public Usuario validate(String user, String password) {
+    public Usuario validate(String user, String passw) {
         Usuario u = null;
         Persona p = null;
         
         PreparedStatement st;
         ResultSet rs;
         String query = null;    
+        
+        try{
+            u = new Usuario();
+            p = new Persona();
+            String hashedPassword = u.HashPassword(passw);
+            query = " select u.id_usuario, u.usuario, p.id_persona,"
+                    + " p.nombre "
+                    + " FROM persona p, usuarios u "
+                    + " where p.id_persona = u.id_persona "
+                    + " AND u.usuario = ? "
+                    + " AND u.password = ?";
+            
+        }catch(Exception e){
+        
+        }finally {
+        
+        }return u;
+        
 }       
+   
 }
 
 
